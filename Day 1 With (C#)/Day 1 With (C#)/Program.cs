8714af0aac93f1e8d03a449388ee925f84981567
Mihir Gupta <mihirkumargupta2@gmail.com>
2024-02-15 22:47:14 +0530

    Working with C#

---
+++ Day 1 With (C#)/Day 1 With (C#)/Program.cs
@@ -0,0 +1,15 @@
+﻿class Variables
+{
+
+  public static void Main(string[] args)
+    {
+        int age = 12;
+        float number = 12.3f;
+        String Name = "Mihir";
+        bool Truth=false;
+        Console.WriteLine(Name);
+        Console.WriteLine(number);
+        Console.WriteLine(age);
+        Console.WriteLine(Truth);
+    }
+}
